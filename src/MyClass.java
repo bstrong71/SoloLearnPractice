6de4fb08bb49153187a16ed9480fe2b40ae4ee86
Bernard Strong <bstrong71@gmail.com>
2017-11-13 16:15:28 -0500

    added getters and setters to vehicle class

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,7 +3,8 @@ public class MyClass {
         Vehicle v1 = new Vehicle();
         Vehicle v2 = new Vehicle();
 
-        v1.color = "red";
+        v1.setColor("red");
+        System.out.println("This vehicle's color is " + v1.getColor() + ".");
 
         v2.horn();
     }
