fa7eae18699a0549f6b3be3c1c1f3f76bcfe9850
Bernard Strong <bstrong71@gmail.com>
2017-11-13 15:59:51 -0500

    initial commit

---
+++ src/MyClass.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle();
+        Vehicle v2 = new Vehicle();
+
+        v1.color = "red";
+
+        v2.horn();
+    }
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,43 @@
+public class Vehicle {
+    private int maxSpeed;
+    private int wheels;
+    private String color;
+    private double fuelCapacity;
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(double fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    void horn() {
+        System.out.println("Beep-Beep!");
+    }
+
+}
