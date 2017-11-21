22764997792193eb144f0131de7e392998cb223d
Bernard Strong <bstrong71@gmail.com>
2017-11-21 14:47:01 -0500

    added counter class

---
+++ src/Counter.java
@@ -0,0 +1,10 @@
+public class Counter {
+
+    // common practice to use upper case when naming static variable (not required)
+    public static int COUNT = 0;
+
+    Counter() {
+        COUNT++;
+    }
+
+}
--- src/MyClass.java
+++ src/MyClass.java
@@ -14,9 +14,17 @@ public class MyClass {
         int previousAge = p1.getAge() - 1;
         System.out.println("John was " + previousAge + ", but now he's " + p1.getAge() + ".");
 
+        Person p2 = new Person("Bill");
+        p2.setAge(40);
+
+        // increments count every time a Counter object is instantiated
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+        System.out.println("Number of Counter objects created: " + Counter.COUNT);
     }
 
     static void celebrateBirthday(Person person) {
         person.setAge(person.getAge() + 1);
     }
+
 }
